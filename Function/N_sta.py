ffcea3bde1c2b3d83bd71db92069d9da236eca75
RenDjunyu <2205997481@qq.com>
2022-04-07 00:09:08 +0800

    full

--- Function/N_sta.py
+++ Function/N_sta.py
@@ -4,27 +4,28 @@ import json
 import re
 
 #N station
-host='v6.nexushd.org'
-origin='https://v6.nexushd.org'
-referer='https://v6.nexushd.org/signin.php'
-url= "https://v6.nexushd.org/signin.php?"
-user="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.56"
-cookie = "c_secure_uid=MTQxMzQ5; c_secure_pass=6af69e7fdf6a066a9610cc3d671a201a; c_secure_ssl=bm9wZQ==; c_secure_tracker_ssl=bm9wZQ==; c_secure_login=bm9wZQ=="
+url= "http://nexushd.org/signin.php?"
+host='www.nexushd.org'
+origin='http://www.nexushd.org'
+referer='http://www.nexushd.org/signin.php'
+cookie = "c_secure_uid=MTQxMzQ5; c_secure_pass=6af69e7fdf6a066a9610cc3d671a201a; c_secure_ssl=bm9wZQ%3D%3D; c_secure_tracker_ssl=bm9wZQ%3D%3D; c_secure_login=bm9wZQ%3D%3D"
 
 header={
-    'accept-encoding':  'gzip, deflate',
-    'accept-language':  'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-    'cache-control':    'max-age=0',
-    'connection':       'keep-alive',
-    'content-length':   '21',
-    'content-type':     'application/x-www-form-urlencoded',
-    'cookie':   cookie,
-    'host':     host,
-    'origin':   origin,
-    'referer':  referer,
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Accept-Encoding':  'gzip, deflate',
+    'Accept-Language':  'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+    'Cache-Control':    'max-age=0',
+    'Connection':       'keep-alive',
+    'Content-Length':   '21',
+    'Content-Type':     'application/x-www-form-urlencoded',
+    'Cookie':   cookie,
+    'Host':     host,
+    'Origin':   origin,
+    'Referer':  referer,
     "Upgrade-Insecure-Requests": "1",
-    'user-agent': user
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.56"
 }
 
-checkin = requests.post(url,headers=header,data=json.dumps({'content': "1"}))
+checkin = requests.post(url,headers=header,data='action=post&content=3')
 print(re.findall("你已经.{10}",checkin.text))
+print(checkin.text)
--- Function/start_up.bat
+++ Function/start_up.bat
@@ -1,10 +1,10 @@
 
-"D:\Common File\TimTencent\Bin\Tim.exe"
-"D:\Common File\WechatTencent\WeChat\WeChat.exe"
-::"D:\dingding\DingtalkLauncher.exe"
-"D:\Common File\CloudMusic\CloudMusic\cloudmusic.exe"
+start "" "D:\Common File\TimTencent\Bin\Tim.exe"
+::start "" "D:\Common File\WechatTencent\WeChat\WeChat.exe"
+::start "D:\dingding\DingtalkLauncher.exe"
+::start "" "D:\Common File\CloudMusic\CloudMusic\cloudmusic.exe"
 ::"D:\Common File\Microsoft VS Code\Code.exe"
-"explorer.exe"
+start "" "explorer.exe"
 
 ::N_station
 python ./N_sta.py
